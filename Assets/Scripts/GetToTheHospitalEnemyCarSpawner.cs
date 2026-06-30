beffd01dad3a9c53c5af8d59d18de2c623d5becb
Mitchell-Sparrow-LAB4-303 <1036314@student.sae.edu.au>
2026-07-01 09:12:34 +1000

    Scene Setup, Main Scripts Added

--- Assets/Scripts/GetToTheHospitalEnemyCarSpawner.cs
+++ Assets/Scripts/GetToTheHospitalEnemyCarSpawner.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GetToTheHospitalPlayerMovement : MonoBehaviour
+public class GetToTheHospitalEnemyCarSpawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,15 +12,5 @@ public class GetToTheHospitalPlayerMovement : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private void moveLeft()
-    {
-
-    }
-
-    private void moveRight()
-    {
-
     }
 }
