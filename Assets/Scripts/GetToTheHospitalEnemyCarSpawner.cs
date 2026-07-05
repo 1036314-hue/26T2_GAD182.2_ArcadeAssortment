192248e574731c13838ad7f004dfd320ad12004b
Mitchell-Sparrow-Home <1036314@student.sae.edu.au>
2026-07-05 19:29:08 +1000

    EnemyCar Spawner Added
    Need to fix spawn frequency

--- Assets/Scripts/GetToTheHospitalEnemyCarSpawner.cs
+++ Assets/Scripts/GetToTheHospitalEnemyCarSpawner.cs
@@ -1,16 +1,40 @@
+using NUnit.Framework;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class GetToTheHospitalEnemyCarSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private GameObject enemyCar;
+    public float spawnTimer = 200f;
+    private Vector3[] enemySpawnPoints =
+    {
+        new Vector3(0, 1.5f, 90),
+        new Vector3(-8, 1.5f, 90),
+        new Vector3(8, 1.5f, 90)
+    };
+
+
+    private Vector3 currentSpawnPoint;
+
+    private bool canSpawn = true;
+
+    
     void Start()
     {
-        
+        StartCoroutine(SpawnCars());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnCars()
     {
-        
+        WaitForSeconds wait = new WaitForSeconds(spawnTimer);
+        while (canSpawn == true)
+        {
+            yield return wait;
+            currentSpawnPoint = enemySpawnPoints[UnityEngine.Random.Range(0, enemySpawnPoints.Length)];
+            Instantiate(enemyCar, currentSpawnPoint, Quaternion.identity);
+            yield return wait;
+        }
     }
 }
